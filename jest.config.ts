db32a15a0d2a876a92c43bf26bcbb8034db28176
Feynman <2901954062@qq.com>
2022-09-03 10:47:19 +0800

    workflow: add jest test 代码格式化配置

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: fg
+ * @Date: 2022-09-03 09:58:53
+ * @LastEditors: fg
+ * @LastEditTime: 2022-09-03 09:58:54
+ * @Description: content
+ */
---
+++ test/tools.test.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: fg
+ * @Date: 2022-09-03 09:54:45
+ * @LastEditors: fg
+ * @LastEditTime: 2022-09-03 09:56:32
+ * @Description: tools test
+ */
